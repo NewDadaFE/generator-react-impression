6fe161f46551347e567db823c90ac4561e83b10e
biped <poticr@gmail.com>
2017-11-23 19:45:52 +0800

    Add fn version

--- app/templates/webpack.config.js
+++ app/templates/webpack.config.js
@@ -2,8 +2,6 @@ const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const { merge, fn, resolve } = require('webpack-fn')
 
-const { npm_package_name: NAME, npm_package_version: VERSION } = process.env
-
 const PATHS = {
   package: resolve('./package.json'),
   entry: resolve('./src/main.js'),
@@ -13,68 +11,7 @@ const PATHS = {
   css: resolve('src/app/styles'),
 }
 
-const common = merge([
-  fn.output({
-    path: PATHS.output,
-    filename: 'scripts/app.js',
-  }),
-  fn.image({
-    limit: 10000,
-    name: 'images/[name].[hash:6].[ext]',
-  }),
-  fn.font({
-    limit: 10000,
-    name: 'fonts/[name].[ext]',
-  }),
-  fn.resolve(PATHS.src),
-  fn.html(PATHS.html),
-  fn.moment(),
-])
-
-const production = merge([
-  fn.entry(PATHS.entry),
-  fn.output({
-    publicPath: `//fe.imdada.cn/${NAME}/${VERSION}/`,
-  }),
-  fn.babel({
-    include: [PATHS.src, /whatwg-fetch/],
-  }),
-  fn.sass({
-    exclude: [PATHS.css, /node_modules/],
-    use: ExtractTextPlugin.extract({
-      fallback: 'style-loader',
-      use: [
-        {
-          loader: 'css-loader',
-          options: {
-            modules: true,
-            importLoaders: 1,
-            localIdentName: '[name]__[local]___[hash:base64:5]',
-          },
-        },
-        'postcss-loader',
-        'sass-loader',
-      ],
-    }),
-  }),
-  fn.sass({
-    include: [PATHS.css, /node_modules/],
-    use: ExtractTextPlugin.extract({
-      fallback: 'style-loader',
-      use: ['css-loader', 'postcss-loader', 'sass-loader'],
-    }),
-  }),
-  fn.stats(),
-  fn.clean(PATHS.output),
-  fn.env({
-    'process.env.NODE_ENV': '"production"',
-    __DEV__: JSON.stringify(false),
-  }),
-  fn.minifyJS(),
-  {
-    plugins: [new ExtractTextPlugin('styles/app.css')],
-  },
-])
+const common = merge([fn.output(), fn.resolve(), fn.html(), fn.moment()])
 
 const development = merge([
   fn.entry([
@@ -83,13 +20,7 @@ const development = merge([
     'webpack/hot/only-dev-server',
     PATHS.entry,
   ]),
-  fn.output({ publicPath: 'http://localhost:8080/' }),
-  fn.eslint({
-    exclude: /node_modules/,
-    options: {
-      failOnError: true,
-    },
-  }),
+  fn.eslint(),
   fn.babel({
     include: [PATHS.src, /whatwg-fetch/],
     options: {
@@ -121,10 +52,10 @@ const development = merge([
       'sass-loader?sourceMap',
     ],
   }),
-  fn.devtool('cheap-module-eval-source-map'),
-  fn.devServer({
-    proxy: require(PATHS.package).proxy || {},
-  }),
+  fn.image(),
+  fn.font(),
+  fn.devtool(),
+  fn.devServer(),
   fn.env({
     __DEV__: JSON.stringify(true),
   }),
@@ -136,5 +67,49 @@ const development = merge([
   },
 ])
 
+const production = merge([
+  fn.entry(PATHS.entry),
+  fn.babel({
+    include: [PATHS.src, /whatwg-fetch/],
+  }),
+  fn.sass({
+    exclude: [PATHS.css, /node_modules/],
+    use: ExtractTextPlugin.extract({
+      fallback: 'style-loader',
+      use: [
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true,
+            importLoaders: 1,
+            localIdentName: '[name]__[local]___[hash:base64:5]',
+          },
+        },
+        'postcss-loader',
+        'sass-loader',
+      ],
+    }),
+  }),
+  fn.sass({
+    include: [PATHS.css, /node_modules/],
+    use: ExtractTextPlugin.extract({
+      fallback: 'style-loader',
+      use: ['css-loader', 'postcss-loader', 'sass-loader'],
+    }),
+  }),
+  fn.image('production'),
+  fn.font('production'),
+  fn.stats(),
+  fn.clean(),
+  fn.env({
+    'process.env.NODE_ENV': '"production"',
+    __DEV__: JSON.stringify(false),
+  }),
+  fn.minifyJS(),
+  {
+    plugins: [new ExtractTextPlugin('styles/app.css')],
+  },
+])
+
 module.exports = env =>
-  env.production ? merge(common, production) : merge(common, development)
+  env.development ? merge(common, development) : merge(common, production)
